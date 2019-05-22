41fa50ed0f0a61cffd746c7c9316a091515d362f
Ramsey Chapin <ramsey@ramseychapin.com>
2019-05-22 06:08:49 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,8 @@
 def reverse_each_word(sentence)
   original_string_array = sentence.split(" ")
   result_string_array = []
-  original_string_array.each do |letter|
-    result_string_array.push
+  original_string_array.each do |word|
+    result_string_array.push(word.reverse)
   end
-  puts result_string_array.join
+  result_string_array.join(" ")
 end
